5b055dbcccf475a41878b4ebf9bcc15489ba6800
Jmeas <james_please@icloud.com>
2019-03-05 14:03:06 -0800

    Fix footer design

--- src/footer.js
+++ src/footer.js
@@ -4,7 +4,7 @@ import './footer.css';
 export default function Footer() {
   return (
     <footer>
-      <a href="https://jamespls.com">My Personal Site</a>
+      <a href="https://jamespls.com">jamespls.com</a> &bull;{' '}
       <a href="https://twitter.com/jmeaspls">Twitter</a>
     </footer>
   );
