e7927a640dc1ac767990f7e1636d7a92b812489d
Jmeas <james_please@icloud.com>
2019-03-04 23:20:01 -0800

    Set up blog basics

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return <div className="App">McIntosh Blog here</div>;
+  }
+}
+
+export default App;
