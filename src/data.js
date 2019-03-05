58da945332bb9a47b0a05889ef4d25744dd11fcd
Jmeas <james_please@icloud.com>
2019-03-05 13:00:23 -0800

    Update image for playlist ordering

--- src/data.js
+++ src/data.js
@@ -4,18 +4,18 @@ import bookmarksSync from './content/2019-03-06-safari-bookmarks-syncing.md';
 
 export default [
   {
-    title: 'Inconsistent Playlist Ordering',
-    url: '/playlist-ordering',
-    imgUrl: 'playlist-ordering.png',
-    markdownUrl: playlistUrl,
-    published: true,
-    publishDate: '2019-03-06',
-  },
-  {
     title: 'Books Same Page Link',
     url: '/books-same-page-link',
     videoUrl: 'books-same-page-link.mp4',
     markdownUrl: samePageLink,
+    published: true,
+    publishDate: '2019-03-06',
+  },
+  {
+    title: 'Inconsistent Playlist Ordering',
+    url: '/playlist-ordering',
+    imgUrl: 'playlist-ordering.png',
+    markdownUrl: playlistUrl,
     published: true,
     publishDate: '2019-03-06',
   },
