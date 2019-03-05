68481eb6a2f440e1a01bf6003b2ea47eb6dbc9de
Jmeas <james_please@icloud.com>
2019-03-05 10:38:50 -0800

    Add favicon; begin updating design

--- src/content/2019-03-06-playlist-ordering.md
+++ src/content/2019-03-06-playlist-ordering.md
@@ -1,6 +1,8 @@
 Playlists on iOS and macOS use a different sorting algorithm. On iOS, playlists that begin with numbers appear
 before playlists that begin with alphabetic characters. On macOS, it is the opposite.
 
+![Playlist Ordering](/2019-03-06-playlist-ordering.png)
+
 ### Why is this a problem?
 
 Sorting logic for the same data should be consistent across platforms unless there's good reason to change it.
--- src/home/header.js
+++ src/home/header.js
@@ -5,13 +5,18 @@ import './header.css';
 export default function Header() {
   return (
     <>
-      <h1 className="blogHeader">
+      <header className="blogHeader">
         <Link to="/" className="blogHeader_link">
-          McIntosh
+          <img src="/logo.png" alt="McIntosh" className="blogHeader_logo" />
         </Link>
-      </h1>
+        <h1 className="blogHeader_text">
+          <Link to="/" className="blogHeader_link">
+            McIntosh
+          </Link>
+        </h1>
+      </header>
       <p className="blogDescription">
-        A blog documenting IA/UX/design issues in iOS + macOS.
+        IA/UX/design notes and issues for iOS/macOS.
       </p>
     </>
   );
